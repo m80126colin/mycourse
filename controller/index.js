e41ccf9234ae9fb02b106be728c214171fa9fa07
m80126colin <m80126colin@gmail.com>
2014-05-21 02:37:17 +0800

    Add model file and write controller

--- controller/index.js
+++ controller/index.js
@@ -1,3 +1,10 @@
-var getOccupation = function(req, res) {
+var db		= require('../model');
+
+module.exports.getOccupation = function(req, res) {
+	var oid		= req.params('oid');
+	var result	= db.getOccupation(oid);
+	var args	= {
+		'title': 'Search ' + result.occupation_name,
+	}
 	res.render('occupation', args);
 }
